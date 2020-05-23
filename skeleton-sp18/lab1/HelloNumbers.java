d479af79fe5b86c71369c53f960cf6b84f751f8c
Wangfuyun1234- <wfy13965l9@gmail.com>
2020-05-23 18:12:58 +0800

    completed lab1

---
+++ skeleton-sp18/lab1/HelloNumbers.java
@@ -0,0 +1,22 @@
+public class HelloNumbers{
+   public static void main(String[] args){
+      /*初始化总值*/
+      int sum = 0;
+      /*初始化x变量*/
+      int x = 0;
+      /*开始循环*/
+      while(x < 10){
+      	  sum = sum + x;
+          System.out.println(sum);
+          x = x + 1;
+      }
+
+
+
+
+   }
+
+
+
+
+}
